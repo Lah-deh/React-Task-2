48f686d1d09f29f8f9af911ac4cacc2c24af487f
lah-deh <succssasokere@gmail.com>
2025-06-23 20:55:38 -1100

    fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/hero.jsx'
 import About from '../components/About.jsx'
 import Focus from '../components/Focus.jsx'
-import Project from '../components/project.jsx'
+import Project from '../components/Project.jsx'
 import Contact from '../components/Contact.jsx'
 import Footer from '../components/Footer.jsx'
 
